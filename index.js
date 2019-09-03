7f91c07de196695eef683dd6c606dcc359972514
Yana Ustsinovich <yustinovich@icloud.com>
2019-09-03 15:45:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file
+// Code your solution in this filefunction findMatching(drivers, string) {
+function findMatching(drivers, string) {
+  let matchingDrivers = drivers.filter(function (match) {return match.toUpperCase() === string.toUpperCase()});
+  return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+
+   let matchingFuzzy = drivers.filter(function (match) {return match.startsWith(string)})
+  return matchingFuzzy;
+}
+
+function matchName(drivers, string) {
+  let matchingDrivers = drivers.filter(function (match) {return match.name === string})
+  return matchingDrivers
+}
